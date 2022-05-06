247f944bf8935b85bed7501b85e3d99774be1774
nikithasrii <98526622+nikithasrii@users.noreply.github.com>
2022-05-07 00:46:53 +0530

    #include<stdio.h> int main() {     int a,b,x,y,i,p;     scanf("%d%d",&a,&b);     if(a>b)     {         x=a;         y=b;     }     else     {         x=b;         y=a;     }     for(i=1;i<x*b;i++)     {         p=x*i;         if(p%y==0)         {             printf("%d",p);             break;         }     } }

--- LCM.c
+++ LCM.c
@@ -1,30 +1,25 @@
-#include <stdio.h>
-
-int main() {
-
-int a,b;
-
-//printf("Enter two numbers 
-");
-scanf("%d",&a); 
-scanf("%d",&b);
-/*
-
-*LCM is always greater than or equel to the-
-*maximum of the given two numbers
-*/
-int lcm =(a>b)?a:b;
-
-//loop until divisible by both
-while(1){
- if (lcm%a==0 && lcm%b==0)
-break;
-lcm++;
-
-}
-
-//output the LCM 
-printf("%d",lcm);
-
-return 0;
+#include<stdio.h>
+int main()
+{
+    int a,b,x,y,i,p;
+    scanf("%d%d",&a,&b);
+    if(a>b)
+    {
+        x=a;
+        y=b;
+    }
+    else
+    {
+        x=b;
+        y=a;
+    }
+    for(i=1;i<x*b;i++)
+    {
+        p=x*i;
+        if(p%y==0)
+        {
+            printf("%d",p);
+            break;
+        }
+    }
 }
